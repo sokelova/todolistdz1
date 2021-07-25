c82350e9d6082aa3f6e5db619a8de4a3b65dbb4f
sokelova <sokelova@gmail.com>
2021-07-25 10:21:36 +0300

    go

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -11,15 +11,29 @@ type AffairPropsType = {
 function Affair(props: AffairPropsType) {
     // const{deleteAffairCallback,affair,}=props
     // const deleteCallback = () => {deleteAffairCallback(affair._id)}// need to fix
-    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}// need to fix
+
+    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}
+
+    let className;
+    if (props.affair.priority === "high") {
+        className = styl.span_name_high;
+    }
+    if (props.affair.priority === "low") {
+        className = styl.span_name_low;
+    }
+    if (props.affair.priority === "middle") {
+        className = styl.span_name_middle;
+    }
 
     return (
         <div>
             <div key={props.affair._id}  className={styl.container}>
-                <span className={styl.span_name}>{props.affair.name + " "}</span>
+                {/*<span className={styl.className}>{props.affair.name + " "}</span>*/}
+                <span className={styl.name}>{props.affair.name + " "}</span>
             </div>
             <div  className={styl.container}>
-                <span className={styl.priority}>{props.affair.priority}</span>
+                {/*<span className={styl.className}>{props.affair.priority}</span>*/}
+                <span className={className}>{props.affair.priority}</span>
             </div>
             <button onClick={deleteCallback}>X</button>
         </div>
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -12,6 +12,7 @@ type AffairsPropsType = { // need to fix any
 function Affairs(props: AffairsPropsType) {
 
     const mappedAffairs = props.data.map((a: AffairType) => (
+
         <Affair // should work
             key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
             affair={a}
